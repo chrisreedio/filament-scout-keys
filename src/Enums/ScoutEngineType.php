a163ba890a6d98b7b6fb9e73d584f5e6e8ed6e51
Chris Reed <chris@reedtech.us>
2024-09-13 16:31:55 -0500

    ✨ feat: added `ScoutEngineType` enum & updated `SearchKeysRelationManager`
    Introduced `ScoutEngineType` enum implementing `HasColor`, `HasIcon`, and `HasLabel`. Enhanced `SearchKeysRelationManager` with new columns and actions using `ScoutEngineType`.

---
+++ src/Enums/ScoutEngineType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ChrisReedIO\ScoutKeys\Filament\Enums;
+
+use Filament\Support\Colors\Color;
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasIcon;
+use Filament\Support\Contracts\HasLabel;
+use ChrisReedIO\ScoutKeys\Enums\ScoutEngineType as BaseEngineType;
+
+enum ScoutEngineType: string implements HasColor, HasIcon, HasLabel
+{
+    case Meilisearch = 'meilisearch';
+    case Typesense = 'typesense';
+
+    public function getColor(): string | array | null
+    {
+        return match ($this) {
+            self::Meilisearch => Color::Violet,
+            self::Typesense => Color::Blue,
+        };
+    }
+
+    public function getIcon(): ?string
+    {
+        return match ($this) {
+            self::Meilisearch, self::Typesense => 'heroicon-o-magnifying-glass',
+        };
+    }
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::Meilisearch => 'Meilisearch',
+            self::Typesense => 'Typesense',
+        };
+    }
+
+    public static function fromEngine(BaseEngineType $engine): ScoutEngineType
+    {
+        return match ($engine) {
+            BaseEngineType::Meilisearch => ScoutEngineType::Meilisearch,
+            BaseEngineType::Typesense => ScoutEngineType::Typesense,
+        };
+    }
+}
--- src/Resources/UserResource/RelationManagers/SearchKeysRelationManager.php
+++ src/Resources/UserResource/RelationManagers/SearchKeysRelationManager.php
@@ -2,9 +2,12 @@
 
 namespace ChrisReedIO\ScoutKeys\Filament\Resources\UserResource\RelationManagers;
 
+use ChrisReedIO\ScoutKeys\Filament\Enums\ScoutEngineType;
+use ChrisReedIO\ScoutKeys\Models\SearchKey;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Support\Colors\Color;
 use Filament\Tables;
 use Filament\Tables\Table;
 
@@ -27,7 +30,25 @@ class SearchKeysRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('uuid')
             ->columns([
-                Tables\Columns\TextColumn::make('uuid'),
+                Tables\Columns\TextColumn::make('uuid')
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('engine')
+                    ->badge()
+                    ->color(fn (SearchKey $key) => ScoutEngineType::fromEngine($key->engine)->getColor())
+                    ->formatStateUsing(fn (SearchKey $key) => ScoutEngineType::fromEngine($key->engine)->getLabel())
+                    ->icon(fn (SearchKey $key) => ScoutEngineType::fromEngine($key->engine)->getIcon()),
+                Tables\Columns\IconColumn::make('is_scoped')
+                    ->label('Scoped')
+                    ->boolean()
+                    ->getStateUsing(fn (SearchKey $key) => $key->scoped_key !== null),
+                Tables\Columns\TextColumn::make('expires_at')
+                    ->formatStateUsing(fn (SearchKey $key) => $key->expires_at->diffForHumans())
+                    ->icon(fn (SearchKey $key
+                    ) => $key->expires_at->isPast() ? 'heroicon-o-exclamation-circle' : 'heroicon-o-check-badge')
+                    ->color(fn (SearchKey $key) => $key->expires_at->isPast() ? Color::Red : Color::Green)
+                    // ->dateTime('Y-m-d H:i:s')
+                    // ->tooltip(fn (SearchKey $key) => $key->expires_at->diffForHumans()),
+                    ->tooltip(fn (SearchKey $key) => $key->expires_at->format('Y-m-d H:i:s')),
             ])
             ->filters([
                 //
@@ -41,13 +62,23 @@ class SearchKeysRelationManager extends RelationManager
                 // Tables\Actions\CreateAction::make(),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                // Tables\Actions\EditAction::make(),
+                // Tables\Actions\DeleteAction::make(),
+                Tables\Actions\Action::make('revoke')
+                    ->label('Revoke')
+                    // ->icon('heroicon-s-backspace')
+                    ->icon('heroicon-s-no-symbol')
+                    ->color('danger')
+                    ->button()
+                    ->action(function (SearchKey $key) {
+                        $key->revoke();
+                        $key->delete();
+                    }),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make(),
+                // ]),
             ]);
     }
 }
