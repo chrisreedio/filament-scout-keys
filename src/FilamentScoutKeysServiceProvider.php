a5e6e8cb9fb8590e0c48dbd644d3413e8aeb1644
chrisreedio <chrisreedio@users.noreply.github.com>
2024-09-13 10:08:51 +0000

    Fix styling

--- src/FilamentScoutKeysServiceProvider.php
+++ src/FilamentScoutKeysServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace ChrisReedIO\ScoutKeys\Filament;
 
+use ChrisReedIO\ScoutKeys\Filament\Commands\FilamentScoutKeysCommand;
+use ChrisReedIO\ScoutKeys\Filament\Testing\TestsFilamentScoutKeys;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use ChrisReedIO\ScoutKeys\Filament\Commands\FilamentScoutKeysCommand;
-use ChrisReedIO\ScoutKeys\Filament\Testing\TestsFilamentScoutKeys;
 
 class FilamentScoutKeysServiceProvider extends PackageServiceProvider
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace ChrisReedIO\ScoutKeys\Filament\Tests;
 
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
+use ChrisReedIO\ScoutKeys\Filament\FilamentScoutKeysServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
 use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
@@ -16,7 +17,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use ChrisReedIO\ScoutKeys\Filament\FilamentScoutKeysServiceProvider;
 
 class TestCase extends Orchestra
 {
